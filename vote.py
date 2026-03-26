8286a6367d6450f3deb54f3e395738bda7d4043b
jaiawasthi-ensylon <jai.awasthi@ensylon.com>
2026-03-26 10:10:24 +0530

    fix: bug_littlehard
    Resolves: SCRUM-12
    Jira: https://jayawasthi891.atlassian.net/browse/SCRUM-12
    
    Patched `vote.py` to fix: In vote.py the boolean literals are written in lowercase (false/true) which are undefined in Python; this will raise a N (9 line(s) changed)

--- vote.py
+++ vote.py
@@ -1,9 +1,8 @@
-can_vote = false
+can_vote = False
 age = int(input("Enter your age"))
-if(age <18):
-  can_vote = true
+if(age >=18):
+  can_vote = True
 if(can_vote):
   print("The user is eligible to vote")
 else:
   print("The user is not eligible to vote")
-  
