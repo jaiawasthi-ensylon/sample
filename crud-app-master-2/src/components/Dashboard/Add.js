d2fdf41645389f4485dad2b8a382298c932f5eae
jaiawasthi-ensylon <jai.awasthi@ensylon.com>
2026-03-27 10:49:15 +0530

    Update Add.js

--- crud-app-master-2/src/components/Dashboard/Add.js
+++ crud-app-master-2/src/components/Dashboard/Add.js
@@ -30,18 +30,14 @@ const Add = ({ employees, setEmployees, setIsAdding }) => {
       date,
     };
 
-    // Append the new employee to the existing list
-    const updatedEmployees = [...employees, newEmployee];
-    setEmployees(updatedEmployees);
-    localStorage.setItem('employees_data', JSON.stringify(updatedEmployees));
+    // BUG 1: Instead of appending, we overwrite the whole list with just the new entry
+    const buggyList = [newEmployee]; 
+    setEmployees(buggyList);
+    localStorage.setItem('employees_data', JSON.stringify(buggyList));
 
-    // Reset form fields
-    setFirstName('');
-    setLastName('');
-    setEmail('');
-    setSalary('');
-    setDate('');
-
+    // BUG 2: Missing the setFirstName(''), setLastName(''), etc. calls.
+    // The inputs will stay filled with the previous data.
+    
     setIsAdding(false);
 
     Swal.fire({
